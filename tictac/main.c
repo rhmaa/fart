5a25ca3e788c6095803e92a058fc5f60acec9c02
Rikard Hevosmaa <rikard@hevosmaa.net>
2022-08-27 16:24:04 +0200

    Clean up code and commments

--- tictac/main.c
+++ tictac/main.c
@@ -9,24 +9,23 @@
 #define NR_ROWS 3
 #define NR_COLS 3
 
-void go_bananas(int [][NR_COLS]);
-void print_board(int [][NR_COLS]);
-void get_move(int *, char *);
-int  check_move(int [][NR_COLS], int, int);
-int  check_win(int [][NR_COLS], int);
+int  go_bananas  (int [][NR_COLS]);
+void print_board (int [][NR_COLS]);
+void get_move    (int *, char *);
+int  check_move  (int [][NR_COLS], int, int);
+int  check_win   (int [][NR_COLS], int);
 
 int main(void)
 {
-    /* This is the board on which the game will be played. We
-     * initialise each cell of the board with a value of 0. A cell
-     * containing 0 means that the player has not yet put down a
-     * symbol.
+    /* This is the board on which the game is played.
      *
-     * 1 means that the player has put down a cross (printed to the
-     * user as X).
+     * All squares on the board are initialised with a value of 0.
      *
-     * 2 means that the player has put down a circle (printed to the
-     * user as O).
+     * A value of 1 means that player 1 has placed their symbol in the
+     * square. This is printed to the user as an 'X'.
+     *
+     * A value of 2 means that player 2 has placed their symbol in the
+     * square. This is printed to the user as an 'O'.
      */
     int board[NR_ROWS][NR_COLS] = {
         { 0, 0, 0 },
@@ -34,53 +33,67 @@ int main(void)
         { 0, 0, 0 }
     };
 
-    go_bananas(board);
+    /* Run the game loop and get the winning player. */
+    int winner = go_bananas(board);
+
+    print_board(board);
+
+    if (winner != 0)
+        printf("Congratulations player %d!\n\n", winner);
+    else
+        printf("It's a draw!\n\n");
 
     return 0;
 }
 
-void go_bananas(int board[][NR_COLS])
+int go_bananas(int board[][NR_COLS])
 {
-    /* player_to_move determines which player's turn it is. 0 means
-     * that it's player 1's turn, 1 means that it's player 2's turn.
+    /* player determines which player's turn it is. A value of 0 means
+     * player 1, a value of 1 means player 2. Confusing, I know.
      */
-    int player_to_move = 0;
+    int player = 0;
+
+    /* row and col are the coordinates of the square in which the
+     * player would like to place their symbol.
+     */
+    int row, col;
+    
+    /* is_legal determines if the player's desired move is legal or
+     * not.
+     */
+    int is_legal = 0;
+
+    /* has_won determines if the current player has won or not. If
+     * they have won, then the game is over.
+     */
+    int has_won = 0;
 
     for (int i = 0; i < (NR_ROWS * NR_COLS); ++i) {
         print_board(board);
+        printf("Player %d, your move!\n\n", player ? 2 : 1);
 
-        printf("\nPlayer %d, your move!\n\n", player_to_move ? 2 : 1);
-
-        int is_legal = 0;
-        int row, col;
-   
-        /* Get the player's move, and make sure that it's legal. */
         do {
             get_move(&row, "row");
             get_move(&col, "column");
             is_legal = check_move(board, row, col);
             if (!is_legal)
-                printf("\n--- Illegal move! Shame on you!!! ---\n\n");
+                printf("Illegal move! Shame on you!!!\n\n");
         } while (!is_legal);
 
-        /* Place the player's symbol on the board. */
-        board[row-1][col-1] = (player_to_move ? 2 : 1);
+        board[row-1][col-1] = player ? 2 : 1;
 
-        /* Check if the player has won. */
-        int has_won = check_win(board, player_to_move);
+        has_won = check_win(board, player);
         
-        if (has_won) {
-            print_board(board);
-            printf("\n--- Congratulations player %d, you have won! ---\n\n", player_to_move ? 2 : 1);
-            break;
-        } else if (i == (NR_ROWS * NR_COLS - 1)) {
-            print_board(board);
-            printf("\n--- It's a draw! ---\n\n");
-            break;
-        } else {
-            player_to_move = player_to_move ? 0 : 1;
-        }
+        if (has_won)
+            return player ? 2 : 1;
+        else
+            player = player ? 0 : 1;
     }
+
+    /* If the loop ended before a player could win, then that means
+     * the game ended in a draw.
+     */
+    return 0;
 }
 
 void print_board(int board[][NR_COLS])
@@ -101,6 +114,7 @@ void print_board(int board[][NR_COLS])
 
         printf("\n  +---+---+---+\n");
     }
+    printf("\n");
 }
 
 void get_move(int *move, char *prompt)
@@ -109,15 +123,12 @@ void get_move(int *move, char *prompt)
     do {
         scanf("%d", move);
         if (!(1 <= *move && *move <= 3))
-            printf("Enter a valid number: ");
+            printf("Enter a valid %s number: ", prompt);
     } while (!(1 <= *move && *move <= 3));
 }
 
 int check_move(int board[][NR_COLS], int row, int col)
 {
-    /* If the cell contains a 0, i.e. if no player has placed a symbol
-     * in the cell previously, then it is a legal move.
-     */
     if (board[row-1][col-1] == 0)
         return 1;
     else
@@ -129,7 +140,7 @@ int check_win(int board[][NR_COLS], int player)
     int symbol = player ? 2 : 1;
 
     /* There are a total of eight win conditions in tic-tac-toe. We
-     * check for each one of them.
+     * check for each one of them here.
      */
     if (/* Symbols are horizontally aligned. */
         (board[0][0] == symbol && board[0][1] == symbol && board[0][2] == symbol) ||
