13608286cdf801486ba8daa6d7e59f948b89a1a1
Rikard Hevosmaa <rikard@hevosmaa.net>
2022-09-04 19:09:48 +0200

    Add hangman

---
+++ hangman/main.c
@@ -0,0 +1,233 @@
+/***********************************************************************
+  A simple hangman game.
+  
+  A list of words are read from a file provided by the player. A
+  random word is picked, and the player has to guess which word it is.
+ ***********************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/* According to Google, the longest word in the English language is 45
+ * characters long.
+ * www.britannica.com/dictionary/eb/qa/The-Longest-Word-in-the-Dictionary
+ */
+#define MAX_WORD_LEN 45
+
+/* The number of chances that the user has to guess the right word. */
+#define NR_CHANCES   8
+
+char ** load_words    (char **, char *);
+char *  get_rand_word (char **);
+int     go_bananas    (char *);
+int     check_guess   (char, char *);
+void    print_word    (char *);
+void    get_guess     (char *);
+void    save_guess    (char, char *, char *);
+
+int main(int argc, char **argv)
+{
+    /* The list of words from which we will pick a random word. */
+    char **words;
+
+    /* The random word that the player has to guess. */
+    char *rand_word;
+
+    /* The full path to the file from which we get our words. */
+    char *filepath;
+
+    if (argc != 2) {
+        printf("usage: %s [FILE]\n", argv[0]);
+        return 1;
+    } else {
+        filepath = calloc(strlen(argv[1]), sizeof(char));
+        if (filepath == NULL) {
+            perror("calloc in main failed");
+            return 1;
+        } else {
+            strncpy(filepath, argv[1], strlen(argv[1]));
+        }
+    }
+
+    /* Load the words from a file and pick one at random. */
+    words = load_words(words, filepath);
+    if (words == NULL) {
+        perror("load_words in main failed");
+        return 1;
+    }
+
+    rand_word = get_rand_word(words);
+
+    /* Run the game loop. */
+    int has_won = go_bananas(rand_word);
+
+    if (has_won)
+        printf("Congratulations, you won!\n\n");
+    else
+        printf("Better luck next time, cunt!\n\n");
+
+    free(filepath);
+    free(rand_word);
+    free(words);
+    
+    return 0;
+}
+
+int go_bananas(char *word)
+{
+    /* The number of chances that the player has used. After each
+     * wrong guess, increment by one.
+     */
+    int chances_used = 0;
+
+    /* The player's guess, compared against every character in
+     * word.
+     */
+    char guess;
+
+    /* All the player's guesses. When guesses == word, the player has
+     * won.
+     */
+    char *guesses;
+
+    guesses = calloc(strlen(word), sizeof(char));
+    if (guesses == NULL) {
+        perror("calloc in go_bananas failed");
+        exit(1);
+    } else {
+        memset(guesses, '_', strlen(word));
+    }
+
+    while (chances_used < NR_CHANCES) {
+        printf("\nYou have %d %s left.\n", NR_CHANCES - chances_used,
+               (NR_CHANCES - chances_used == 1) ? "chance" : "chances");
+
+        print_word(guesses);
+        printf("Your guess: ");
+        get_guess(&guess);
+
+        /* Only save the guess if it's actually correct. */
+        if (check_guess(guess, word) == 0)
+            save_guess(guess, guesses, word);
+        else
+            ++chances_used;
+
+        if (strcmp(guesses, word) == 0) {
+            printf("\n");
+            print_word(guesses);
+            free(guesses);
+            return 1;
+        }
+    }
+
+    free(guesses);
+
+    return 0;
+}
+
+int check_guess(char guess, char *word)
+{
+    for (int i = 0; i < strlen(word); ++i)
+        if (guess == word[i])
+            return 0;
+
+    return 1;
+}
+
+void save_guess(char guess, char *guesses, char *word)
+{
+    for (int i = 0; i < strlen(word); ++i)
+        if (guess == word[i])
+            guesses[i] = guess;
+}
+
+void get_guess(char *guess)
+{
+    char buffer[MAX_WORD_LEN];
+
+    fgets(buffer, MAX_WORD_LEN, stdin);
+
+    if (strlen(buffer) != 2) {
+        printf("\nYou should only enter one character, you naughty boy.\n");
+        return;
+    } else {
+        buffer[strcspn(buffer, "\n")] = 0;
+        guess[0] = buffer[0];
+    }
+}
+
+void print_word(char *guesses)
+{
+    for (int i = 0; i < strlen(guesses); ++i)
+        printf("%c ", guesses[i]);
+
+    printf("\n");
+}
+
+char ** load_words(char **words, char *filepath)
+{
+    /* The file from which we read the list of words. */
+    FILE *fp;
+
+    /* The number of lines in the text file. */
+    int nr_lines = 0;
+
+    /* A character read from the file. c is compared against '\n',
+     * which is how we determine where a line ends.
+     */
+    int c = 0;
+
+    /* A word read from the file, stored here temporarily before being
+     * saved to words.
+     */
+    char word[MAX_WORD_LEN];
+
+    fp = fopen(filepath, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "fopen failed to open the file %s", filepath);
+        return NULL;
+    }
+
+    /* Get the number of lines in the file. Using this method means
+     * that the last word in the file has to end with a newline
+     * character, otherwise it will not be counted.
+     */
+    for (c = getc(fp); c != EOF; c = getc(fp))
+        if (c == '\n')
+            ++nr_lines;
+
+    words = malloc(sizeof(char *) * nr_lines);
+    if (words == NULL) {
+        perror("malloc in load_words failed");
+        return NULL;
+    }
+
+    rewind(fp);
+
+    for (int i = 0; i < nr_lines; ++i) {
+        fgets(word, MAX_WORD_LEN, fp);
+        words[i] = calloc(strlen(word), sizeof(char));
+        if (words[i] == NULL) {
+            fprintf(stderr, "calloc failed to allocate words[%d]\n", i);
+            return NULL;
+        } else {
+            word[strcspn(word, "\n")] = 0;
+            strncpy(words[i], word, strlen(word));
+        }
+    }
+
+    fclose(fp);
+    return words;
+}
+
+char * get_rand_word(char **words)
+{
+    srand(time(NULL));
+
+    int nr_words   = sizeof(*words) / sizeof(*words[0]);
+    int rand_index = rand() % nr_words;
+
+    return words[rand_index];
+}
